dc818588d967c61f6e164cf11c14c00d08050d69
MonicaAuditore <mauditore@live.it>
2023-02-08 16:14:09 +0100

    add_store test

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,23 @@
 <script>
 import AppSearch from "./AppSearch.vue";
+import { store } from "../store";
 
 export default {
   name: "AppHeader",
   components: { AppSearch },
   data() {
-    return {};
+    return {
+      store,
+    };
   },
 
   methods: {
     testMess(parametro, num) {
       console.log("hey", parametro, num);
+    },
+
+    incrementa() {
+      this.store.counter++;
     },
   },
 };
@@ -18,6 +25,8 @@ export default {
 
 <template>
   <AppSearch message="ciao" @eventodalfiglio="testMess" />
+  <h2>{{ store.counter }}</h2>
+  <button @click="incrementa">btn header</button>
 </template>
 
 <style scoped></style>
